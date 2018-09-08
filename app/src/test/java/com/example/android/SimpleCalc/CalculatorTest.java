a46780eeb22c85cafafd5cebd42039c2ef5cf5e6
murrayep <eugene.murray@btinternet.com>
2018-09-08 09:46:54 +0100

    Initial commit

---
+++ app/src/test/java/com/example/android/SimpleCalc/CalculatorTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2016, Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.SimpleCalc;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * JUnit4 unit tests for the calculator logic. These are local unit tests; no device needed
+ */
+@RunWith(JUnit4.class)
+@SmallTest
+public class CalculatorTest {
+
+    private Calculator mCalculator;
+
+    /**
+     * Set up the environment for testing
+     */
+    @Before
+    public void setUp() {
+        mCalculator = new Calculator();
+    }
+
+    /**
+     * Test for simple addition
+     */
+    @Test
+    public void addTwoNumbers() {
+        double resultAdd = mCalculator.add(1d, 1d);
+        assertThat(resultAdd, is(equalTo(2d)));
+//        assertThat(resultAdd, is(equalTo(3d)));
+    }
+
+    @Test
+    public void addTwoNumbersNegative() {
+        double resultAdd = mCalculator.add(-1d, 2d);
+        assertThat(resultAdd, is(equalTo(1d)));
+    }
+
+    @Test
+    public void addTwoNumbersFloats() {
+        double resultAdd = mCalculator.add(1.111f, 1.111d);
+//        assertThat(resultAdd, is(equalTo(2.222d)));
+        assertThat(resultAdd, is(closeTo(2.222, 0.01)));
+    }
+
+    @Test
+    public void subTwoNumbers() {
+        double resultSub = mCalculator.sub(3d, 1d);
+        assertThat(resultSub, is(equalTo(2d)));
+    }
+    @Test
+    public void subWorksWithNegativeResults() {
+        double resultSub = mCalculator.sub(1d, -3d);
+        assertThat(resultSub, is(equalTo(4d)));
+    }
+    @Test
+    public void mulTwoNumbers() {
+        double resultMul = mCalculator.mul(2d, 3d);
+        assertThat(resultMul, is(equalTo(6d)));
+    }
+    @Test
+    public void mulTwoNumbersZero() {
+        double resultMul = mCalculator.mul(2d, 0d);
+        assertThat(resultMul, is(equalTo(0d)));
+    }
+    @Test
+    public void divTwoNumbers() {
+        double resultDiv = mCalculator.div(4d, 2d);
+        assertThat(resultDiv, is(equalTo(2d)));
+    }
+    @Test
+    public void divByZero() {
+        double resultDiv = mCalculator.div(4d, 0d);
+        assertThat(resultDiv, is(equalTo(Double.POSITIVE_INFINITY)));
+    }
+}
